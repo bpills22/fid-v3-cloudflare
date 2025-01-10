a83cb052d62ea67019835a7dca05b9c54c8f4b79
Brian Pillsbury <122814506+bpills22@users.noreply.github.com>
2025-01-09 21:22:18 -0500

    updating finalized CF Worker code

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["assets-flightaware.bpillsbury.com"], // Add your external domain here
+    domains: ["assets-flightaware.bpillsbury.com"], // Retain your external image domain
+    unoptimized: true, // Required for static export
   },
   reactStrictMode: true,
+  output: "export", // Enable static export
 };
 
 export default nextConfig;
