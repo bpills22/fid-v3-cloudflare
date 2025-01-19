027cc96440190a7819521286d00519720fdb99f0
Brian Pillsbury <122814506+bpills22@users.noreply.github.com>
2025-01-19 11:41:31 -0500

    added Atlanta to app

--- src/app/page.js
+++ src/app/page.js
@@ -185,6 +185,7 @@ export default function HomePage() {
             onChange={handleSelectChange}
             displayEmpty
           >
+            <MenuItem value="katl">Atlanta - KATL - ATL</MenuItem>
             <MenuItem value="kaus">Austin - KAUS - AUS</MenuItem>
             <MenuItem value="kbos">Boston - KBOS - BOS</MenuItem>
             <MenuItem value="kbur">Burbank - KBUR - BUR</MenuItem>
